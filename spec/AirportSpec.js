916e7c10d6fcd9f0a5a532d3a58d5e50fefae17b
VladC <vlad.chiriac24@gmail.com>
2019-05-30 15:18:17 +0100

    Checks weather conditions

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -24,4 +24,15 @@ Describe( 'Airport', function() {
 		expect(airport.planes()).toEqual([]);
 	});
 
+	it('can check for stormy coditions', function() {
+		expect(airport.isStormy()).toBeFalsy();
+	});
+
+	describe('under stormy conditions', function() {
+		it('does not clear planes for takeoff', function(){
+			spyOn(airport, 'isStormy').and.returnValue(true);
+			expect(function(){airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+		});
+	});
+
 });
--- src/Airport.js
+++ src/Airport.js
@@ -15,3 +15,14 @@ Airport.prototype.clearForLanding = function(plane) {
 Airport.prototype.clearForTakeOff = function(plane) {
 	this._hangar = [];
 };
+
+Airport.prototype.isStormy = function() {
+	return false;
+};
+
+Airport.prototype.clearForTakeOff = function(plane) {
+	if(this.isStormy()) {
+		throw new Error('cannot takeoff during storm');
+	}
+	this._hangar =[];
+};
